90324e809ee5c6a07ea7a9c84c3d01c29fca84b0
RyotaBannai <ryotala0528@gmail.com>
2020-05-31 17:36:15 +0900

    set up vuex learning

---
+++ vuex-app/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>{{ count }}</div>
+    <button v-on:click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+
+  },
+  // state
+  data () {
+    return {
+      msg: 'Counter App',
+      count: 0
+    }
+  },
+  // actions
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
